dd3f9842ee940265df17de35064546e5fafc317b
krishnarao-yallampalli <72746140+krishnarao-yallampalli@users.noreply.github.com>
2020-12-10 18:13:22 +0530

    JSX Expressions JSX Expressions used in the script.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(
+const name = "Krishna Rao";
+const luckyNo = 7;
+
+const myElement = (
   <div>
-    <h1>Favorite Heros</h1>
-    <ul>
-      <li>Jesus</li>
-      <li>Paul</li>
-      <li>Peter</li>
-    </ul>
-  </div>,
-  document.getElementById("root")
+    <h1>Hello {name}!</h1>
+    <p>My Lucky number is {luckyNo}</p>
+  </div>
 );
+
+ReactDOM.render(myElement, document.getElementById("root"));
