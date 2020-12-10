a3467b00433e45330967e0e6c64fac3e517d35df
krishnarao-yallampalli <72746140+krishnarao-yallampalli@users.noreply.github.com>
2020-12-10 16:18:08 +0530

    Challenge1 Challenge 1 completed.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from "react-dom";
 
 ReactDOM.render(
   <div>
-    <h1>Hello World!</h1>
-    <p>This is a paragraph.</p>
+    <h1>Favorite Heros</h1>
+    <ul>
+      <li>Jesus</li>
+      <li>Paul</li>
+      <li>Peter</li>
+    </ul>
   </div>,
   document.getElementById("root")
 );
