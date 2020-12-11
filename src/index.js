d44c426fcb1cf6ac292974754d7c28bca18071cd
krishnarao-yallampalli <72746140+krishnarao-yallampalli@users.noreply.github.com>
2020-12-11 14:25:58 +0530

    Function Components Function Components

--- src/index.js
+++ src/index.js
@@ -1,27 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-var date = new Date();
-var currentTime = date.getHours();
-
-var greeting;
-
-const myStyle = {
-  color: ""
-};
-
-if (currentTime < 12) {
-  greeting = "Good Morning";
-  myStyle.color = "red";
-} else if (currentTime < 18) {
-  greeting = "Good Afternoon";
-  myStyle.color = "green";
-} else {
-  greeting = "Good Evening";
-  myStyle.color = "blue";
-}
+import Header from "./Components/Heading";
+import Fruits from "./Components/Fruits";
 
 ReactDOM.render(
-  <h1 style={myStyle}>{greeting} </h1>,
+  <div>
+    <Header />
+    <Fruits />
+  </div>,
   document.getElementById("root")
 );
